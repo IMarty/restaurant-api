35fbc7efd5ba6cf80359fb11d77465d169f263ff
IMarty <igor.marty@telecomnancy.net>
2023-06-26 17:40:25 +0200

    cors allowing

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI, HTTPException, status, Header
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 restaurant_list= [
     {"name": "Mac Donalds", "likes": 10, "currentlyOpen": True},
     {"name": "Burger King", "likes": 15, "currentlyOpen": True},
